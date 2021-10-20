81ab5a5ddc8bc457d0518fe3bc0c0ad7298c85a0
David-Ade-Creator <www.davidadeyemi@gmail.com>
2021-10-20 15:04:12 +0530

    changed baseUrl to production url

--- src/store/baseUrl.js
+++ src/store/baseUrl.js
@@ -1,2 +1,2 @@
-// export const baseUrl = "https://queaserver.herokuapp.com";
-export const baseUrl = "http://localhost:1100";
+export const baseUrl = "https://queaserver.herokuapp.com";
+// export const baseUrl = "http://localhost:1100";
